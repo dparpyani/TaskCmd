822cd36f4293b1f93740b440a358d2946603d079
Deepak Parpyani <dparpyani@yahoo.com>
2014-12-31 19:01:09 -0500

    Create file for 'complete' command.

---
+++ src/commands/complete.js
@@ -0,0 +1,8 @@
+var output = require('../output');
+
+module.exports.run = function(params, storage) {
+    output.error('Not implemented yet!');
+    params.forEach(function(id) {
+        // TODO
+    });
+};
